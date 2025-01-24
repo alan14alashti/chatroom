13046c758e8c5ccd160be66277093f35e269e927
ali14taghavi <ali14taghavi@gmail.com>
2025-01-24 15:16:49 +0330

    hash password and check password implemeted in auth package

---
+++ internal/auth/password.go
@@ -0,0 +1,17 @@
+package auth
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword encrypts a password
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hash), err
+}
+
+// CheckPassword compares a plain password with a hashed one
+func CheckPassword(hashedPassword, plainPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+	return err == nil
+}
